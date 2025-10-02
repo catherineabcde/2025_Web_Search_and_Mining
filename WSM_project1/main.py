16399f946510a0d923db88c3bbdfb809937c64d4
catherine <c109700019.mg09@nycu.edu.tw>
2025-10-02 11:05:14 +0800

    feat: complete task 1-1

--- WSM_project1/main.py
+++ WSM_project1/main.py
@@ -9,6 +9,7 @@ import VectorSpace as vs
 import Parser
 import os
 import tfidf
+import util
 
 eng_path = "./EnglishNews/"
 eng_txts = [f for f in os.listdir(eng_path) if f.endswith(".txt")]
@@ -23,7 +24,7 @@ for txt in eng_txts:
         eng_filename.append(txt)
     f.close()
 
-eng_vs = vs.VectorSpace(eng_docs)
+eng_vec = vs.VectorSpace(eng_docs)
 
 """
 how many documents are there? object.documentVectors
@@ -33,7 +34,28 @@ how many words are there in these documents? object.vectorKeywordIndex
 # (1) TF Weighting (Raw TF in course PPT) + Cosine Similarity
 # retrieve the top 10 relevant docs
 query = "planet Taiwan typhoon"
-tf_score = {}
+query_vec = eng_vec.buildQueryVector(query)
+
+# Consine Similarity
+similarity = {}
+for i, doc_vec in enumerate(eng_vec.documentVectors):
+    similarity[eng_filename[i]] = util.cosine(doc_vec, query_vec)
+
+top_10 = sorted(similarity.items(), key=lambda x: x[1], reverse=True)[:10]
+
+# formulate
+print("TF Cosine")
+print(f"{'NewsID':<20} {'Score':<15}")
+for filename, score in top_10:
+    print(f"{filename:<20} {score:<15f}")
+print("-" * 35)
+
+# (2) TF-IDF Weighting + Cosine Similarity
+# retrieve the top 10 relevant docs
+
+
+
+    
 
 
 
